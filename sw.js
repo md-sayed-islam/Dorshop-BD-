a1166f51c9366fa03a10c54fd26e8b047a221b1f
md-sayed-islam <sayed266112025@gmail.com>
2026-08-06 23:17:35 +0600

    Implement service worker caching and event listeners

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
+const CACHE='dorshopbd-v6-SECRET-FINAL';
+self.addEventListener('install',e=>{ self.skipWaiting(); console.log('SW v6 install'); });
+self.addEventListener('activate',e=>{ 
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>{
+      console.log('Delete cache',k);
+      return caches.delete(k);
+    })))
+  ); 
+  self.clients.claim(); 
+});
+self.addEventListener('fetch',e=>{ 
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))); 
+});
 
