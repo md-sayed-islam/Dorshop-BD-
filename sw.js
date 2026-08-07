6de738701156c362addf83c373570ded55acf38b
md-sayed-islam <sayed266112025@gmail.com>
2026-08-07 17:35:01 +0600

    Add service worker for cache management
    Implement service worker to unregister old cache and skip waiting.

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-
+// Dorshop BD - No Cache - Unregisters old cache
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.claim())
+  );
+});
