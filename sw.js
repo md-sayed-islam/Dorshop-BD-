64fcbc1f1db62805dbe14f077c3756b79cc01f7c
md-sayed-islam <sayed266112025@gmail.com>
2026-08-08 22:04:07 +0600

    Add service worker event listeners
    Implement service worker events for install, activate, and fetch.

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
+// Dorshop BD v16 Firebase REAL LIVE - NO CACHE - Project dorshop-bd-5f7ff
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  console.log('Dorshop BD v16 REAL LIVE - deleting old v3 cache');
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request, {cache: 'no-store'}));
+});
 
